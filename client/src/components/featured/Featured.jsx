6aad3ae88c960b4235c3c86f85c58440172b083e
ithaqua <hiepnguyentuan1002@gmail.com>
2022-03-24 17:41:13 +0700

    build client navbar - fix port in api

---
+++ client/src/components/featured/Featured.jsx
@@ -0,0 +1,10 @@
+import ".fe./featured.scss"
+
+export default function Featured() {
+  return (
+    <div className="featured">
+        <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" 
+        alt="" />
+    </div>
+  )
+}
